b1a4fc0f19002858bf59eb0dee445d838eeed51a
gauravdesale8 <gauravdesale08@gmail.com>
2026-04-12 21:49:10 +0530

    add detail to model

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
 
-    name: str
-    age: int
-    weight: float
+    name: str = Field(max_length = 50)
+    email: EmailStr
+    lnkindUrl: AnyUrl
+    age: int = Field(gt=0, lt=120)
+    weight: float = Field(gt=0)
     married: bool
-    allergies: List[str]
+    allergies: Optional[List[str]] = Field(max_length=5) and None
     contact_details: Dict[str, str]
 
 
@@ -16,10 +18,14 @@ def insert_patient_data(patient: Patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
     print("Inserted into databases.")
 
 
-patient_info = {'name':'Ramesh', 'age':30}
+patient_info = {'name':'Ramesh','email':'abc@gmail.com','lnkindUrl':'http://google.com/2343', 'age':30, 'weight':55.7, 'married': True, 'allergies': ['dust','pollen'], 'contact_details': {'email':'abc@gmail.com','phone':'234560'}}
 
 patient1 = Patient(**patient_info)
 
